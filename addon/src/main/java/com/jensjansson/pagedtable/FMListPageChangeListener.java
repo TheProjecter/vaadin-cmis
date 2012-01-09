830a7374a6a65901f933a32d9ed149d3d538617d
aaron <aaron@fatminds.com>
2012-01-09 08:48:25 +0000

    removed cruft, deleted unnecessary repositories and pluginrepositories from pom.xml

--- addon/src/main/java/com/jensjansson/pagedtable/FMListPageChangeListener.java
+++
@@ -1,7 +0,0 @@
-package com.jensjansson.pagedtable;
-
-import com.jensjansson.pagedtable.PagedTable.PageChangeListener;
-
-public interface FMListPageChangeListener extends PageChangeListener {
-
-}
